a381f697f58f5f4547f23daa0e0d978208e941cb
darren-kk <rlawoghks10@gmail.com>
2023-08-15 19:09:48 +0900

    Feat. add login controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const Users = require("../models/User");
+const CONFIG = require("../configs/index");
+
+exports.login = async function (req, res, next) {
+  const { email } = req.body;
+
+  try {
+    const user = await Users.findOne({ email: email });
+    const token = jwt.sign({ id: user._id }, CONFIG.SECRET_KEY);
+
+    res.cookie("AccessToken", token, {
+      httpOnly: true,
+    });
+
+    res.status(201).json({ user });
+  } catch (error) {
+    error.status = 500;
+    error.message = "Internal Server Error";
+    next(error);
+  }
+};
+
+exports.signup = async function (req, res, next) {
+  try {
+    const hashedPwd = await bcrypt.hash(req.body.password, Number(CONFIG.SALT));
+
+    const newUser = await Users.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPwd,
+    });
+
+    res.status(201).json({ newUser });
+  } catch (error) {
+    error.status = 500;
+    error.message = "Internal Server Error";
+    next(error);
+  }
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
 const authController = require("../controllers/auth.controller");
+const validateSignup = require("../middlewares/validateSignup");
+const validateLogin = require("../middlewares/validateLogin");
 
 const router = express.Router();
 
-router.post("/login", authController.login);
-router.post("/signup", authController.signup);
+router.post("/login", validateLogin, authController.login);
+router.post("/signup", validateSignup, authController.signup);
 
 module.exports = router;
