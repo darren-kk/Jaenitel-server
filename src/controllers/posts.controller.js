430efe6e51a34c77b67f9a963f39011f493eff96
darren-kk <rlawoghks10@gmail.com>
2023-08-18 00:35:02 +0900

    Feat. add create post

---
+++ src/controllers/posts.controller.js
@@ -0,0 +1,80 @@
+const { getS3Client, getListObjectsCommand, getPutObjectCommand } = require("../configs/s3Config");
+
+const User = require("../models/User");
+const TextContent = require("../models/TextContent");
+const ImageContent = require("../models/ImageContent");
+const VideoContent = require("../models/VideoContent");
+const Post = require("../models/Post");
+
+const CONFIG = require("../configs/index");
+
+exports.createPost = async (req, res, next) => {
+  const { userId } = req.params;
+  const s3Client = getS3Client();
+  const contents = req.body.contents;
+
+  // imageUrl: `https://${CONFIG.AWS_S3_BUCKET_NAME}.s3.${CONFIG.AWS_S3_REGION}.amazonaws.com/posts/${user._id}/${imageFileName}`,
+  try {
+    const user = await User.findById(userId);
+    const uploadPromises = [];
+
+    for (const file of req.files) {
+      const indexPattern = /\[(\d+)\]/;
+      const match = file.fieldname.match(indexPattern);
+      const index = match ? parseInt(match[1], 10) : -1;
+
+      if (file.fieldname.includes("imageContent")) {
+        const id = new Date().toISOString();
+        const fileName = `posts/${user._id}/${id}.png`;
+        const putObjectCommand = getPutObjectCommand(CONFIG.AWS_S3_BUCKET_NAME, fileName, file.buffer, "image/png");
+        const url = `https://${CONFIG.AWS_S3_BUCKET_NAME}.s3.${CONFIG.AWS_S3_REGION}.amazonaws.com/${fileName}`;
+        const uploadPromise = s3Client.send(putObjectCommand);
+
+        uploadPromises.push(uploadPromise);
+
+        contents[index] = { imageContent: url };
+      }
+
+      if (file.fieldname.includes("videoContent")) {
+        const id = new Date().toISOString();
+        const fileName = `posts/${user._id}/${id}.mp4`;
+        const putObjectCommand = getPutObjectCommand(CONFIG.AWS_S3_BUCKET_NAME, fileName, file.buffer, "video/mp4");
+        const url = `https://${CONFIG.AWS_S3_BUCKET_NAME}.s3.${CONFIG.AWS_S3_REGION}.amazonaws.com/${fileName}`;
+        const uploadPromise = s3Client.send(putObjectCommand);
+
+        uploadPromises.push(uploadPromise);
+
+        contents[index] = { videoContent: url };
+      }
+    }
+
+    await Promise.all(uploadPromises);
+
+    const savedContents = await Promise.all(
+      contents.map((content) => {
+        if (content.textContent) {
+          return new TextContent({ content: content.textContent });
+        }
+        if (content.imageContent) {
+          return new ImageContent({ content: content.imageContent });
+        }
+        if (content.videoContent) {
+          return new VideoContent({ content: content.videoContent });
+        }
+      }),
+    );
+
+    const newPost = new Post({
+      title: req.body.title,
+      category: req.body.category,
+      madeBy: user._id,
+      contents: savedContents,
+    });
+
+    await newPost.save();
+
+    res.json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+};
