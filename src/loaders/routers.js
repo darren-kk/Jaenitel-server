6d2efe072e5d73514c9290701f672f211e3c60ea
darren-kk <rlawoghks10@gmail.com>
2023-08-29 05:35:51 +0900

    Fix. add / router for deploy

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -1,7 +1,9 @@
 const authRouter = require("../routes/auth");
 const usersRouter = require("../routes/users");
+const indexRouter = require("../routes/index");
 
 async function routerLoader(app) {
+  app.use("/", indexRouter);
   app.use("/auth", authRouter);
   app.use("/users", usersRouter);
 }
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", async function (req, res, next) {
+  res.send("intial");
+});
