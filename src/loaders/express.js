04391f0460a86cd6a4173401872a0c56fc71e232
darren-kk <rlawoghks10@gmail.com>
2023-08-30 04:18:03 +0900

    Feat. add nginx cofing to edit max body size after deploy

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -10,8 +10,10 @@ async function expressLoader(app) {
       credentials: true,
     }),
   );
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+
   app.use(cookieParser());
 }
 
