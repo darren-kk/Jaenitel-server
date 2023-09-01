b7d4bf58298adb16a044a8075dc5b046296b5e81
darren-kk <rlawoghks10@gmail.com>
2023-09-01 14:48:59 +0900

    Fix. fix getContentType function to accept every file type

--- src/utils/getContentType.js
+++ src/utils/getContentType.js
@@ -1,18 +1,23 @@
 function getContentType(fileName) {
+  const contentTypeMapping = {
+    jpg: "image/jpeg",
+    jpeg: "image/jpeg",
+    png: "image/png",
+    gif: "image/gif",
+    bmp: "image/bmp",
+    tiff: "image/tiff",
+    webp: "image/webp",
+    mp4: "video/mp4",
+    webm: "video/webm",
+    mov: "video/quicktime",
+    avi: "video/x-msvideo",
+    wmv: "video/x-ms-wmv",
+  };
+
   const extension = fileName.split(".").pop().toLowerCase();
+  const contentType = contentTypeMapping[extension];
 
-  switch (extension) {
-    case "png":
-      return { contentType: "image/png", extension: ".png" };
-    case "jpg":
-    case "jpeg":
-      return { contentType: "image/jpeg", extension: ".jpg" };
-    case "mp4":
-      return { contentType: "video/mp4", extension: ".mp4" };
-
-    default:
-      return null;
-  }
+  return { contentType, extension: `.${extension}` };
 }
 
 module.exports = getContentType;
